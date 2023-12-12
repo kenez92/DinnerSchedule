655131a53b6f2d63aa2253b62402e26f1b4824f5
Daniel <kenez92@gmail.com>
2023-12-12 21:41:16 +0100

    init project

---
+++ src/main/java/pl/kenez/DinnerScheduleApplication.java
@@ -0,0 +1,13 @@
+package pl.kenez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinnerScheduleApplication.class, args);
+    }
+
+}
